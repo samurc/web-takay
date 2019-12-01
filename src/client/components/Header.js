cc4769ddf792c812815eb6d39ea32c5271abe1c1
Samuel Cusi <scusi@gruporpp.com.pe>
2019-12-01 14:09:21 -0500

    first commit

---
+++ src/client/components/Header.js
@@ -0,0 +1,79 @@
+/* eslint-disable no-script-url */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  const [menuOpen, setMenuToggle] = useState(false);
+
+  const toggleMenu = () => setMenuToggle(!menuOpen);
+
+  const stylesOpen = {
+    transform: 'translateX(0px)'
+  };
+  return (
+    <div className="navbar-fixed">
+      <nav className="orange">
+        <div className="container">
+          <div className="nav-wrapper">
+            <a href="/" className="brand-logo">
+              TAKAY
+            </a>
+            <a href="javascript:void(0)" onClick={toggleMenu} className="sidenav-trigger right">
+              <i className="material-icons">menu</i>
+            </a>
+            <div
+              className="sidenav-overlay"
+              style={menuOpen ? { display: 'block', opacity: 1 } : null}
+              onClick={toggleMenu}
+            />
+            <ul id="slide-out" className="sidenav" style={menuOpen ? stylesOpen : null}>
+              <li>
+                <a className="subheader">Menu</a>
+              </li>
+              <li>
+                <div className="divider" />
+              </li>
+              <li>
+                <Link to="/" className="item" onClick={toggleMenu}>
+                  FAMILIA
+                </Link>
+              </li>
+              <li>
+                <Link to="/articles/techradar" className="item" onClick={toggleMenu}>
+                  EXPERTO
+                </Link>
+              </li>
+              <li>
+                <Link to="/articles/mashable" className="item" onClick={toggleMenu}>
+                  NOSOTROS
+                </Link>
+              </li>
+            </ul>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li>
+                <Link to="/articles/techradar" className="item">
+                  FAMILIA
+                </Link>
+              </li>
+              <li>
+                <Link to="/articles/mashable" className="item">
+                  EXPERTO
+                </Link>
+              </li>
+              <li>
+                <Link to="/articles/the-verge" className="item">
+                  NOSOTROS
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
