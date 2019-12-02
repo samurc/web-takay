8c0c96a51fd9b6171c4b0aecf2b0b7123832f2e0
Samuel Cusi <scusi@gruporpp.com.pe>
2019-12-01 20:18:16 -0500

    Interes

--- src/client/components/Interes/index.js
+++ src/client/components/Interes/index.js
@@ -14,8 +14,8 @@ const Info = () => {
         </Card>
         <Card className="card">
           <i className="material-icons">emoji_people</i>
-          <h2>Quiero construir</h2>
-          <p>Mi familia y yo tenemos un proyecto de construcción</p>
+          <h2>Quiero prestar mis servicios</h2>
+          <p>Soy un experto en construcción</p>
         </Card>
       </LayoutRow>
     </Grid>
