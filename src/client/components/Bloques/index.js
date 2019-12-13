e1101e76eb3e58bc34615bda205f5713417e9ecf
jean pierre <jpsantos3003@gmail.com>
2019-12-12 23:43:00 -0500

    cambios

--- src/client/components/Bloques/index.js
+++ src/client/components/Bloques/index.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { Grid, LayoutRow, Card } from './styles';
-import imgFamily from '../../assets/img/familia.png';
-import imgExperto from '../../assets/img/experto.png';
+import imgFamily1 from '../../assets/img/nosotros1.png';
+import imgFamily2 from '../../assets/img/nosotros2.png';
+import imgFamily3 from '../../assets/img/nosotros3.png';
 
 const Bloques = () => {
   return (
     <Grid className="grid">
       <LayoutRow>
         <Card className="card">
-          <img src={imgFamily} width={84} height={84} />
+          <img src={imgFamily1} width={84} height={84} />
           <h2>¿Qué hacemos?</h2>
         </Card>
         <Card className="card">
-          <img src={imgFamily} width={84} height={84} />
+          <img src={imgFamily2} width={84} height={84} />
           <h2>¿Por qué lo hacemos?</h2>
         </Card>
         <Card className="card">
-          <img src={imgExperto} width={60} height={84} />
+          <img src={imgFamily3} width={60} height={84} />
           <h2>¿Quiénes somos?</h2>
         </Card>
       </LayoutRow>
